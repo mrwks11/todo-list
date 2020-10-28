f2092fa8802c509d5e5978d922b3c16b0bea5c56
mrwks11 <w.k.sinke@gmail.com>
2020-10-28 09:05:13 +0100

    Create new project and render in DOM.

--- src/index.js
+++ src/index.js
@@ -1,118 +1,45 @@
-// LOCAL STORAGE
-let projects = [];
-let tasks = [];
-
 // SELECTORS
-// Projects
 const projectInputTitle = document.getElementById('project-title');
 const projectCreateButton = document.querySelector('.project-create-button');
 const projectListContainer = document.querySelector('.project-list-container');
+const projectList = document.querySelector('.project-list');
 
-//Tasks
 const taskInputTitle = document.getElementById('task-title');
 const taskInputDescription = document.getElementById('task-description');
 const taskInputDueDate = document.getElementById('task-date');
 const taskInputPriority = document.getElementById('task-priority');
 const taskCreateButton = document.querySelector('.task-create-button');
 const taskListContainer = document.querySelector('.task-list-container');
-
-// EVENT LISTENERS
-// Projects
-projectCreateButton.addEventListener('click', addNewProject);
-
-// Tasks
-taskCreateButton.addEventListener('click', addNewTask);
+const taskList = document.querySelector('.task-list');
 
 // FUNCTIONS
-// Add New Project
-function addNewProject(e) {
-  // Prevent Form Submit
+function createNewProjectHandler(e) {
   e.preventDefault();
-  // Check User Input
-  if (projectInputTitle.value == null || projectInputTitle.value === '') return;
+  if (projectInputTitle.value === null || projectInputTitle.value === '')
+    return;
   const newProject = createNewProject();
-  // Add To Project List
-  projects.push(newProject);
-  saveNewProject();
   renderNewProject();
-  resetUserInput();
+  clearUserInput();
 }
 
-// Add New Task
-function addNewTask(e) {
-  // Prevent Form Submit
-  e.preventDefault();
-  // Check User Input
-  if (taskInputTitle.value == null || taskInputTitle.value === '') return;
-  const newTask = createNewTask();
-  // Add To Task List
-  tasks.push(newTask);
-  saveNewTask();
-  renderNewTask();
-  resetUserInput();
-}
-
-// Save New Project in Local Storage
-
-// Save New Task in Local Storage
-
-// Render New Project in DOM
-function renderNewProject() {
-  const newProject = document.createElement('ul');
-  newProject.classList.add('project-list-item');
-  const newProjectTitle = document.createElement('li');
-  newProjectTitle.innerText = projectInputTitle.value;
-  // Append New Project
-  newProject.appendChild(newProjectTitle);
-  projectListContainer.appendChild(newProject);
-}
-
-// Render New Task in DOM
-function renderNewTask() {
-  const newTask = document.createElement('ul');
-  newTask.classList.add('task-list-item');
-  const newTaskTitle = document.createElement('li');
-  newTaskTitle.innerText = taskInputTitle.value;
-  // const newTaskDescription = document.createElement('li');
-  // newTaskDescription.innerText = taskInputDescription.value;
-  // const newTaskDueDate = document.createElement('li');
-  // newTaskDueDate.innerText = taskInputDueDate.value;
-  // const newTaskPriority = document.createElement('li');
-  // newTaskPriority.innerText = taskInputPriority.value;
-  // Append New Task
-  newTask.appendChild(newTaskTitle);
-  // newTask.appendChild(newTaskDescription);
-  // newTask.appendChild(newTaskDueDate);
-  // newTask.appendChild(newTaskPriority);
-  taskListContainer.appendChild(newTask);
-}
-
-// Reset User Input
-function resetUserInput() {
-  // Project
-  projectInputTitle.value = null;
-  // Task
-  taskInputTitle.value = null;
-  taskInputDescription.value = null;
-  taskInputDueDate.value = null;
-  taskInputPriority.value = null;
-}
-
-// Create New Project
 function createNewProject() {
   return {
     title: projectInputTitle.value,
+    tasks: [],
     // id: Date.now().toString,
   };
 }
 
-// Create New Task
-function createNewTask() {
-  return {
-    title: taskInputTitle.value,
-    description: taskInputDescription.value,
-    date: taskInputDueDate.value,
-    priority: taskInputPriority.value,
-    // id: Date.now().toString,
-  };
+function renderNewProject() {
+  const newProjectListElement = document.createElement('li');
+  newProjectListElement.classList.add('project-list-item');
+  newProjectListElement.innerText = projectInputTitle.value;
+  projectList.appendChild(newProjectListElement);
 }
+
+function clearUserInput() {
+  projectInputTitle.value = null;
+}
+
+// EVENTS
+projectCreateButton.addEventListener('click', createNewProjectHandler);
